11242da668ced0517c0a0e234019ba5fa3a4fd21
Erik Poort <erik@mediamonks.com>
2017-07-27 16:01:07 -0300

    Add initial native classes

---
+++ ios/RNWalletModule/RNWalletModule.h
@@ -0,0 +1,13 @@
+//
+//  RNWalletModule.h
+//  RNWalletModule
+//
+//  Created by Erik Poort on 27/07/2017.
+//  Copyright (c) 2017 MediaMonks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNWalletModule : NSObject
+
+@end
---
+++ ios/RNWalletModule/RNWalletModule.m
@@ -0,0 +1,13 @@
+//
+//  RNWalletModule.m
+//  RNWalletModule
+//
+//  Created by Erik Poort on 27/07/2017.
+//  Copyright (c) 2017 MediaMonks. All rights reserved.
+//
+
+#import "RNWalletModule.h"
+
+@implementation RNWalletModule
+
+@end
